795b9791d09eff3236ac2d981a4e349c733afe62
Brendon Page <brendon.page@chillisoft.co.za>
2021-03-23 13:46:37 +0200

    Updated implementation of 05_fake_timers.ts to always use ZA locale to prevent the time tests from failing if people have their clock configured to 24h.

--- src/tests-to-implement/05_fake_timers.ts
+++ src/tests-to-implement/05_fake_timers.ts
@@ -2,7 +2,7 @@ export function generateDayMessage() {
   const today = new Date()
   const day = today.getDay()
   const dayName = getDays()[day]
-  return `[${today.toLocaleTimeString()}]: Today is ${dayName}`
+  return `[${today.toLocaleTimeString('en-ZA')}]: Today is ${dayName}`
 }
 
 function getDays() {
